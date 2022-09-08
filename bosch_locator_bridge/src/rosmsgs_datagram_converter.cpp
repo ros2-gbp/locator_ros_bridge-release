1faff8ef1bdbcf93f802b9fdf2acd124e2f4b066
Stefan Laible <stefan.laible@de.bosch.com>
2022-09-08 12:58:44 +0200

    Rebase from 'upstream'

--- bosch_locator_bridge/src/rosmsgs_datagram_converter.cpp
+++ bosch_locator_bridge/src/rosmsgs_datagram_converter.cpp
@@ -84,6 +84,10 @@ size_t RosMsgsDatagramConverter::convertMapDatagram2Message(
     bytes_parsed += 8;
     point_cloud.push_back(pt);
   }
+
+  // Discard the extension part of the datagram
+  bytes_parsed += discardExtension(binary_reader);
+
   // Create message
   pcl::toROSMsg(point_cloud, out_pointcloud);
   out_pointcloud.header.frame_id = MAP_FRAME_ID;
@@ -248,6 +252,9 @@ size_t RosMsgsDatagramConverter::convertClientLocalizationVisualizationDatagram2
   std::vector<uint64_t> sensor_offsets = readSensorOffsets(binary_reader);
   readIntensities(binary_reader);
 
+  // Discard the extension part of the datagram
+  discardExtension(binary_reader);
+
   // Use sensor offsets to colorize point cloud
   colorizePointCloud(point_cloud, sensor_offsets);
 
@@ -309,6 +316,9 @@ size_t RosMsgsDatagramConverter::convertClientMapVisualizationDatagram2Message(
   // Get sensor offsets and read intensities
   std::vector<uint64_t> sensor_offsets = readSensorOffsets(binary_reader);
   readIntensities(binary_reader);
+
+  // Discard the extension part of the datagram
+  discardExtension(binary_reader);
 
   // Use sensor offsets to colorize point cloud
   colorizePointCloud(point_cloud, sensor_offsets);
@@ -374,6 +384,9 @@ size_t RosMsgsDatagramConverter::convertClientRecordingVisualizationDatagram2Mes
   // Get sensor offsets and read intensities
   std::vector<uint64_t> sensor_offsets = readSensorOffsets(binary_reader);
   readIntensities(binary_reader);
+
+  // Discard the extension part of the datagram
+  discardExtension(binary_reader);
 
   // Use sensor offsets to colorize point cloud
   colorizePointCloud(point_cloud, sensor_offsets);
@@ -568,6 +581,19 @@ void RosMsgsDatagramConverter::colorizePointCloud(
       point_cloud[i].b = 207;
     }
   }
+}
+
+size_t RosMsgsDatagramConverter::discardExtension(Poco::BinaryReader & binary_reader)
+{
+  uint32_t extensionSize {0u};
+  binary_reader >> extensionSize;
+
+  const auto bytesToDiscard = extensionSize - 4u;
+  std::vector<char> dataToDiscard(bytesToDiscard);
+
+  binary_reader.readRaw(dataToDiscard.data(), bytesToDiscard);
+
+  return extensionSize;
 }
 
 void RosMsgsDatagramConverter::readIntensities(Poco::BinaryReader & binary_reader)
